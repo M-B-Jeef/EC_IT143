85d007023e758f69ab2c1a285bf45516d4019c08
M-B-Jeef <beltrandlebien@gmail.com>
2026-02-17 03:48:30 +0100

    Add files via upload

---
+++ project6.3/EC_IT143_6.3_fwf_s2_MB.sql
@@ -0,0 +1,6 @@
+-- Current status:
+-- I have the table t_w3_schools_customers with ContactName in "First Last" format.
+
+-- Next logical step:
+-- 1. Test using LEFT, CHARINDEX, and SUBSTRING to extract first name.
+-- 2. Compare results to full ContactName to ensure correctness.
